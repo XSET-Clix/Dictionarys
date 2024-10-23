9ae17f07a99339311555c57f2fb67bfdac5a1c10
XSET-Clix <shahded2124@gmail.com>
2024-10-23 19:30:14 -0400

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+print("dictionary")
+fortnitepro = {"name": "Coolest2124", "age": 13, "grade": 7, "food": "pizza"}
+print(fortnitepro["name"])
+print(fortnitepro["age"])
+print(fortnitepro["grade"])
+print(fortnitepro["food"])
+fortnitepro["school"]="Montgomery"
+print(fortnitepro["school"])
+print(fortnitepro)
+fortnitepro["food"]="skittles"
+print(fortnitepro)
+del fortnitepro["grade"]
+print(fortnitepro)
